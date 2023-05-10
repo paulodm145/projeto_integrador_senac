fc49e5e764585fff2f9681a7ecfee54e865edfc6
VMHering <viniciushering10@gmail.com>
2023-05-10 20:57:47 -0300

    Limpeza de layout. Ajustes nas variáveis de sessão

--- php/decode.php
+++ php/decode.php
@@ -38,8 +38,12 @@ include('conexao.php');
 if($pass_valid == 1){
     $_SESSION['login'] = $mail_decode;
     $_SESSION['pass'] = $pass_decode;
-    $id = mysqli_fetch_array(mysqli_query($conn,"SELECT user_id FROM usuarios WHERE email = '$mail_decode'"))[0];
-    $_SESSION['id'] = $id;
+    //$sql = "SELECT user_id FROM usuarios WHERE email = '$mail_decode'";
+    $sql = "SELECT user_id, name, email FROM usuarios WHERE email = '$mail_decode'";
+    $result = mysqli_query($conn, $sql);
+    $dados = mysqli_fetch_array($result);
+    $_SESSION['dados_usuario'] = $dados; 
+    $_SESSION['id'] = $dados[0];
     mysqli_query($conn, "INSERT INTO `acess_logs`(`usuario`, `acess_date`, `acess_time`, `way`) VALUES ('$id',now(), now(), 'IN')");
     header('Location:../index.php');
 }else{
