32cb839ce8c0af8db2914a8a28fac3212114ee4e
Paulo Roberto Bolsanello <pr2desenvolvimento@gmail.com>
2023-05-10 18:24:57 -0300

    feat/login : Ajustes de exemplo códigos de login

--- php/conexao.php
+++ php/conexao.php
@@ -6,9 +6,6 @@ if(!$pass_decode) {
 
 $conn = mysqli_connect('localhost', 'root', '', 'proj_senac');
 
-
-
-
 $pass_valid = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM usuarios WHERE pass = md5('$pass_decode') AND email = '$mail_decode'"));
 
 ?>
