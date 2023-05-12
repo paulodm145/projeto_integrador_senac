e0a172dbdc05680cc7a8f04068cce520bd78c86f
Paulo Roberto Bolsanello <pr2desenvolvimento@gmail.com>
2023-05-12 20:17:49 -0300

    ajustar listagem

--- php/conexao.php
+++ php/conexao.php
@@ -5,7 +5,6 @@ if(!$pass_decode) {
 }
 
 $conn = mysqli_connect('localhost', 'root', '', 'projeto');
-
 $pass_valid = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM usuarios WHERE pass = md5('$pass_decode') AND email = '$mail_decode'"));
 
 ?>
